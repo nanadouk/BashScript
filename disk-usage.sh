b754430b73d0158976e796d50f568b584703f6b1
davide1995 <davide95.v@gmail.com>
2018-10-07 23:22:31 +0200

    Script improved

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
-DIRS="/home /var/log"
-echo "DISK SPACE USAGE"
+echo "================================"
+echo "Assignement 3 - DISK SPACE USAGE"
+echo "Group 7 - Authors:"
+echo "Doukmak Anna"
+echo "Vanoni Davide"
+echo "Fischer Marcus"
+echo "BFH 2018 - Modern Linux"
+echo "================================"
 
+DIRS="/home /var/log"
+MAX_SHOW=5
+
+#Iterate over the specified directories
 for dir in $DIRS; do
-	echo "Directory $dir"
+	#Simple print
+	printf "\nDirectory $dir\n"
+
+	#Formatted print
 	printf "Rating\tSize\tLocation\n"
-	du -S -h $dir 2>/dev/null |
-	sort -rn |
-	sed '{11,$D; =}' |
+
+	#Get directory size in human mode (M, K, ...)
+	du -h --max-depth=1 $dir 2>/dev/null |
+
+	#Sort the list by directory size in descending mode
+	sort -rh |
+
+	#Max number in result list
+	sed '{'$(( $MAX_SHOW+1 ))',$D; =}' |
+
+	#All in one line and not new line after the enumerated id
 	sed 'N; s/\n/ /' |
+
+	#Print the row (directory) in a formatted mode respecting the header
 	awk '{printf $1 "." "\t" $2 "\t" $3 "\n"}'
 done
 exit
