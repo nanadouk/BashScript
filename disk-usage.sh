0f13d43fa162c5e5ff9c7fc60e8a3f1f61d1b386
davide1995 <davide95.v@gmail.com>
2018-10-08 10:48:05 +0200

    Directories taken as argument and ask the max number of directories to show

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$1" == "-h" ]; then
+  echo "Usage: `basename $0` {directory ...}"
+  exit 0
+fi
+
 echo "================================"
 echo "Assignement 3 - DISK SPACE USAGE"
 echo "Group 7 - Authors:"
@@ -9,11 +14,11 @@ echo "Fischer Marcus"
 echo "BFH 2018 - Modern Linux"
 echo "================================"
 
-DIRS="/home /var/log"
-MAX_SHOW=5
+echo "Specify the max number of directories to show:"
+read -r MAX_SHOW
 
-#Iterate over the specified directories
-for dir in $DIRS; do
+#Iterate over arguments
+for dir in "$@"; do
 	#Simple print
 	printf "\nDirectory $dir\n"
 
@@ -35,4 +40,7 @@ for dir in $DIRS; do
 	#Print the row (directory) in a formatted mode respecting the header
 	awk '{printf $1 "." "\t" $2 "\t" $3 "\n"}'
 done
+
+printf "\nGOODBYE!!\n"
+
 exit
